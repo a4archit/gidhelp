73acff4f5de5c828345e608fd48a4fb4c85e4302
Archit <archittyagi599@gmail.com>
2025-02-11 15:44:39 +0530

    adding files

---
+++ gidhelp.py
@@ -0,0 +1,126 @@
+"""
+
+Gidhelp (Github Description Helper):
+==================================
+
+    Documentation updated on: 11/02/2025
+
+    
+    Introduction:
+    ------------
+        Gidhelp is a tool that helps us to write our Github profile description.
+        It is based on next word predictor technique.
+        It has several versions i.e., Gidhelp_75, Gidhelp_500, etc
+        currently there are available only one model Gidhelp_75
+        more models will comming soon.
+
+        
+    How It Works:
+    ------------
+        In the working of these models first of all we apply Tokenization on text afterthat converting
+        data into a supervised labeled data (input and output fully labeled) and then apply Padding 
+        to all the sequences.
+        
+    
+    Currently Available Models:
+    -------------------------
+        -> Gidhelp_75
+        -> Gidhelp_500 (upcomming)
+
+"""
+
+
+
+# files addresses
+GIDHELP_75_MODEL_PATH = "/home/archit-elitebook/workarea/whole working/deep learning/gidhelp/gidhelp_75.h5"
+GIDHELP_75_TOKENIZER_PATH = "/home/archit-elitebook/workarea/whole working/deep learning/gidhelp/gidhelp_75_tokenizer.pkl"
+
+
+
+# importing utilities and packages
+from tensorflow import keras
+from typing import List, Tuple, AnyStr, SupportsInt
+from keras.preprocessing.sequence import pad_sequences
+from numpy import argmax
+from time import sleep 
+
+import pickle 
+
+
+
+
+
+
+class Gidhelp_75:
+
+
+    def __init__(self):
+        self.model = keras.models.load_model(GIDHELP_75_MODEL_PATH)         # loading trained model
+        self.tokenizer_75 = pickle.load(open(GIDHELP_75_TOKENIZER_PATH, 'rb'))   # fecthing tokenizer
+        
+        
+
+
+
+    def predict(self,
+        text: AnyStr,
+        is_return: bool = True,
+        prediction_words_length: SupportsInt = 5,
+        verbose = False
+    ) -> Tuple[str, List]:
+
+        """ This function will print a tuple of string [complete prediction] and a list [prediction words] """
+        
+        # initializing and declaring some useful variables
+        predicted_words = []
+        predicted_text = ""
+
+        # displaying user provided text
+        if verbose == True:
+            print(text.capitalize(), end=" ")
+
+        updated_text = text 
+        for i in range(prediction_words_length):                # predicting `n` - numbers of words
+
+            # tokenizing
+            text_tokenized = self.tokenizer_75.texts_to_sequences([updated_text])[0]
+
+            # applying padding on text
+            text_padded = pad_sequences([text_tokenized], maxlen=23, padding='pre')
+
+            # prediction 
+            pred = self.model.predict(text_padded, verbose=False)
+
+            # getting most probably word
+            word_position: int = argmax(pred)
+
+            # getting suitable word
+            for word, index in self.tokenizer_75.word_index.items():
+                if index == word_position:
+                    updated_text += " " + word                  # updating text for next word prediction
+                    predicted_text += word + " "                # updating predicted text 
+                    predicted_words.append(word)                # updating predicted words list
+                    if verbose:
+                        sleep(0.2)
+                        print(word, end=" ")
+            if verbose:
+                print()
+
+        if is_return:
+            return (predicted_text, updated_text, predicted_words)
+
+
+
+
+
+if __name__ == "__main__":
+
+    model = Gidhelp_75()
+
+    return_material = model.predict("myself archit tyagi ")
+
+    print(return_material)
+
+
+
+
---
+++ gidhelp_main.py
@@ -0,0 +1,108 @@
+
+# importing utilities and packages
+from gidhelp import Gidhelp_75
+from time import sleep 
+
+import streamlit as st
+
+
+
+# Model working
+model = Gidhelp_75()                                                # creating model instance
+
+# some preworking
+st.cache_data.clear()
+st.cache_resource.clear()
+st.set_page_config("Gidhelp (Github Description Helper)", page_icon="📝")
+
+
+# page layout
+st.title("Gidhelp")
+st.markdown("###### :orange[Gi]thub :orange[D]escription :orange[Help]er")
+
+
+st.text("You can use this tool for writing your Github profile description, it may be helpful for you, for more details slide left top-bar")
+
+
+
+details = """#### About the Model
+- Gidhelp is a tool that helps us to write our Github profile description.
+- It is based on next word predictor technique.
+- It has several versions i.e., *Gidhelp_75*, *Gidhelp_500*, etc
+- Currently there are available only one model *Gidhelp_75*
+- More models will comming soon.
+"""
+
+#  Sidebar  
+st.sidebar.title("Menu")
+st.sidebar.divider()
+words_limit = st.sidebar.slider(
+    "Set suggestion word limit",
+    min_value=1,
+    max_value=10,
+    step = 1,
+    value=5
+)
+st.sidebar.markdown(details)
+st.sidebar.write("You can check my social media accounts: ")
+st.sidebar.write("[Website](https://a4archit.github.io/my-portfolio)")
+st.sidebar.write("[Kaggle](https://www.kaggle.com/architty108)")
+st.sidebar.write("[Github](https://www.github.com/a4archit)")
+st.sidebar.write("[LinkedIn](https://www.linkedin.com/in/archit-tyagi-191323296)")
+
+
+
+
+
+
+
+
+# declaring functions
+def clear_text():
+    st.session_state.user_input_text = ""
+    
+def update_input_label():
+    st.session_state.user_input_text = complete_text
+
+# taking user input
+user = st.text_area(
+    "", 
+    key="user_input_text",
+    placeholder="Write something...",
+    help="Write something about your Github profile."
+)
+
+
+# adding buttons
+col1, col2, col3 = st.columns([1,2,1])
+
+with col1:
+    st.button("Clear", on_click=clear_text, use_container_width=True)
+
+with col2:
+    st.button("Update input label", on_click=update_input_label, use_container_width=True)
+
+with col3:
+    st.button("Help me now!", use_container_width=True)
+
+space_for_output = st.empty()
+
+
+predicted_text, complete_text, words_list = model.predict(user, prediction_words_length=words_limit)     # prediction from user text
+# updating output text
+if user != "":
+    output_chars = ""
+    for word_index, word in enumerate(words_list):
+        for index, char in enumerate(word):
+            if index == 0 and word_index != 0:
+                output_chars += f" {char}"
+            else:
+                output_chars += f"{char}"
+
+            space_for_output.markdown(f'<p style="font-size: 1.2em;background-color: #edebeb; padding: 30px; box-sizing:border-box; border-radius: 10px;"><b>{user.capitalize().strip()}</b> <i style="color: #807d7d">{output_chars}</i> </p>', unsafe_allow_html=True)
+            if word_index == 0 and index == 0:
+                sleep(0.5)
+            sleep(0.05)
+
+
+
